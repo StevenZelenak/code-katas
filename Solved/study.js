bff3a571e1ccc42faec4027475c3d84e037ed273
StevenZelenak <stevenmzelenak@gmail.com>
2020-01-13 17:46:53 -0800

    adding study program, it is where I practice solutions

---
+++ Solved/study.js
@@ -0,0 +1,25 @@
+let favoriteresturant = 'Chilis';
+let favoriteFood = 'Nachos';
+
+console.log(`I can't wait to eat ${favoriteFood} at ${favoriteresturant}.`)
+
+function callFood()
+{
+    console.log(`I can't wait to eat ${favoriteFood} at ${favoriteresturant}.`)
+}
+
+callfoodz = () => console.log(`I can't wait to eat ${favoriteFood} at ${favoriteresturant}.`);
+
+callFood();
+callfoodz();
+
+// array lesson on .puhs()
+
+let arrayoffood = ['orange', 'cherry','strawberry'];
+
+
+arrayoffood.push('potatoe');
+
+console.log(arrayoffood);
+console.log(arrayoffood[0].length);
+console.log(arrayoffood.length);
